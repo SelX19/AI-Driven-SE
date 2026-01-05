7aca8b7d31f7bddd349924bc0add97512f3337bc
SelX19 <selma.dozic@stu.ibu.edu.ba>
2026-01-05 17:24:24 +0100

    revert: sidebar responsive changes

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ const Sidebar = ({ isOpen, toggleSidebar, onSelectFilter }) => {
       {/* Overlay */}
       {isOpen && (
         <div
-          className="fixed inset-0 bg-black bg-opacity-50 z-40 lg:hidden"
+          className="fixed inset-0 bg-black bg-opacity-50 z-40"
           onClick={toggleSidebar}
         ></div>
       )}
@@ -17,11 +17,11 @@ const Sidebar = ({ isOpen, toggleSidebar, onSelectFilter }) => {
       <div
         className={`fixed top-0 left-0 w-64 bg-white h-full shadow-lg transform ${
           isOpen ? 'translate-x-0' : '-translate-x-full'
-        } transition-transform duration-300 ease-in-out z-50 lg:relative lg:translate-x-0 lg:shadow-none lg:w-auto`}
+        } transition-transform duration-300 ease-in-out z-50`}
       >
         <div className="p-4 border-b flex items-center justify-between">
           <h2 className="text-xl font-semibold">Menu</h2>
-          <button onClick={toggleSidebar} className="lg:hidden text-gray-600 hover:text-gray-900">
+          <button onClick={toggleSidebar} className="text-gray-600 hover:text-gray-900">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-6 w-6"
